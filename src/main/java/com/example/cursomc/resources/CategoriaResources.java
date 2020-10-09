db692e0bee688dacd70b108503c491450218fb35
Diogonrl <diogonrl@hotmail.com>
2020-10-09 20:29:22 -0300

    teste dominio - Categoria

--- src/main/java/com/example/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/example/cursomc/resources/CategoriaResources.java
@@ -1,15 +1,25 @@
 package com.example.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
-	
+
 	@RequestMapping(method=RequestMethod.GET)
-	public String Listar() {
-		return "Rest Funcionando";
+	public List<Categoria> Listar() {
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "escritorio");
+		List<Categoria> lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
