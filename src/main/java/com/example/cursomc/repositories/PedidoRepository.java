03bdedc61d97a202c75bad7cde4d6a11ad02fc32
Diogonrl <diogonrl@hotmail.com>
2020-10-26 20:20:46 -0300

    Busca de produtos por nome e categoria

--- src/main/java/com/example/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/example/cursomc/repositories/PedidoRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.example.cursomc.domain.Pedido;
 
 @Repository
-public interface PedidoRepository extends JpaRepository<Pedido,Integer> {
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
 }
